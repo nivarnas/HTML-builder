986dddca60c0d5040e172e93bca103f3dc1e2612
nivarnas <rpankratoff@gmail.com>
2024-01-14 20:23:02 +0300

    feat: add solution for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = require('process');
+
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+readStream.on('data', data => {
+  stdout.write(data);
+});
