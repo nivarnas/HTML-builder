6aa558c4331394fe3b97249ab7b661b42263b79a
nivarnas <rpankratoff@gmail.com>
2024-01-22 23:39:22 +0300

    feat: add solution for 6th task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,13 @@
 const path = require('path');
-const { mkdir, copyFile, rm, readdir, access, writeFile } = require('fs/promises');
+const {
+  mkdir,
+  copyFile,
+  rm,
+  readdir,
+  access,
+  writeFile,
+  readFile,
+} = require('fs/promises');
 const fs = require('fs');
 
 const projectFolder = path.resolve(__dirname, 'project-dist');
@@ -7,6 +15,9 @@ const assetsTarget = path.resolve(__dirname, 'assets');
 const coppiedAssets = path.resolve(__dirname, 'project-dist', 'assets');
 const stylesFolder = path.resolve(__dirname, 'styles');
 const bundleStyles = path.resolve(__dirname, 'project-dist', 'style.css');
+const htmlComponentsDir = path.resolve(__dirname, 'components');
+const template = path.resolve(__dirname, 'template.html');
+const htmlFile = path.resolve(projectFolder, 'index.html');
 
 const copyAssets = async (target, copy) => {
   const removeDir = async () => {
@@ -33,10 +44,10 @@ const copyAssets = async (target, copy) => {
     makeDir();
   }
 
-  const files = await readdir(target, {withFileTypes: true});
+  const files = await readdir(target, { withFileTypes: true });
 
   files.forEach(async (file) => {
-    if (file.isDirectory()){
+    if (file.isDirectory()) {
       const newTarrget = path.resolve(target, file.name);
       const newCopy = path.resolve(copy, file.name);
       copyAssets(newTarrget, newCopy);
@@ -47,11 +58,57 @@ const copyAssets = async (target, copy) => {
       try {
         copyFile(fileName, copyFileName);
       } catch (err) {
-        console.log(fileName, copyFileName)
+        console.log(fileName, copyFileName);
         console.log(err);
       }
     }
   });
 };
 
-copyAssets(assetsTarget, coppiedAssets);
+const mergeStyles = async () => {
+  try {
+    const files = await readdir(stylesFolder, { withFileTypes: true });
+    const writeStraem = await fs.createWriteStream(bundleStyles);
+    files.forEach((file) => {
+      const readStream = fs.createReadStream(
+        path.join(stylesFolder, path.basename(file.name)),
+      );
+      if (path.extname(file.name) === '.css' && !file.isDirectory()) {
+        readStream.pipe(writeStraem, { end: false });
+      }
+    });
+  } catch (err) {
+    mergeStyles();
+    console.log(err.message);
+  }
+};
+
+const replaseTemplate = async () => {
+  try {
+    const templateFile = await readFile(template, 'utf-8');
+    const components = await readdir(htmlComponentsDir);
+    let newTemplate = templateFile;
+    for (const component of components) {
+      const componentName = component.split('.')[0];
+      if (component.split('.')[1] === 'html') {
+        const componentContent = await readFile(
+          path.resolve(htmlComponentsDir, component),
+          'utf-8',
+        );
+        const regex = new RegExp(`{{${componentName}}}`, 'g');
+        newTemplate = newTemplate.replaceAll(regex, componentContent);
+      }
+    }
+    await writeFile(htmlFile, newTemplate, 'utf-8');
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+const build = async () => {
+  await copyAssets(assetsTarget, coppiedAssets);
+  await mergeStyles();
+  await replaseTemplate();
+};
+
+build();
